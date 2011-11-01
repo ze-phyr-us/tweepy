06187fe3c5b880ded3f71f216f74b58947e08664
Joshua Roesslein <jroesslein@gmail.com>
2011-11-01 00:00:39 -0500

    Update dependencies for 2.0.
    - Going to use Requests library for HTTP.
    - No longer keep copy of OAuth library in code base, it's now a dep.

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@
 from setuptools import setup, find_packages
 
 setup(name="tweepy",
-      version="1.8",
-      description="Twitter library for python",
+      version="2.0",
+      description="Twitter API Library",
       license="MIT",
       author="Joshua Roesslein",
       author_email="tweepy@googlegroups.com",
       url="http://github.com/tweepy/tweepy",
       packages = find_packages(),
+      requires = [
+          'requests',
+          'oauth >= 1.0.1',
+      ],
       keywords= "twitter library",
       zip_safe = True)
