f3a856a5ca6710bacd393c7003dbf4e77a9d9590
Joshua Roesslein <jroesslein@gmail.com>
2011-11-12 17:17:04 -0600

    Add generic Error.

--- tweepy/__init__.py
+++ tweepy/__init__.py
@@ -5,3 +5,22 @@ __license__ = 'MIT'
 from tweepy.rest_api import API
 from tweepy.streaming import Stream
 
+class Error(RuntimeError):
+    """Tweepy generic error"""
+
+    def __init__(self, message, http_error=None):
+        self._message = message
+        self._http_error = http_error
+
+    @property
+    def message(self):
+        """A hack to get around the deprecation errors in 2.6."""
+        return self._message
+
+    @property
+    def http_error(self):
+        return self._http_error
+
+    def __str__(self):
+        return self._message
+
