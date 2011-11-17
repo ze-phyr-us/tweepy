36ba9c253da6a87c736986f9f5f2ddb0eb28a076
Joshua Roesslein <jroesslein@gmail.com>
2011-11-16 23:58:07 -0600

    Basic JSON parsing working.

--- tweepy/client.py
+++ tweepy/client.py
@@ -1,13 +1,41 @@
 import requests
 from urlparse import urljoin
 
-from tweepy import Error
+from tweepy import TweepyError
+from tweepy.parsers import default_json_parser
 
 class Client(object):
 
-    def __init__(self, auth=None, host='api.twitter.com', secure=True, api_version='1'):
+    def __init__(self,
+        auth=None,
+        host='api.twitter.com',
+        secure=True,
+        api_version='1',
+        parser=default_json_parser,
+        response_format='json'):
+        """
+        auth: a tuple that takes the format: (auth_mode, param_1, param_2, ..., param_n)
+              Supported modes: OAuth, Basic
+        host: hostname of the API server. Defaults to api.twitter.com
+        secure: if true uses HTTPS for network requests to the API server. Default is true.
+        api_version: which version of the API to use. Defaults to 1.
+        parser: an instance of the parser which extends the Parser class.
+                By default the JSONParser is used with default configurations.
+        response_format: the response format to request. Ex: json(default), xml, rss, atom
+                         The parser must support the selected response format.
+        """
         self.base_url = '%s://%s/%s' % ('https' if secure else 'http', host, api_version)
-        self.session = requests.session(auth=auth) if auth else requests
+
+        if isinstance(auth, tuple):
+            self.session = requests.session(auth=auth)
+        else:
+            self.session = requests
+
+        # Make sure parser supports response format.
+        if response_format not in getattr(parser, 'accepted_formats', ['json']):
+            raise TweepyError('Parser does not support the response format %s' % response_format)
+        self.parser = parser
+        self.response_format = response_format
 
     def request(self, method, url, parameters=None):
         """Send a request to API server.
@@ -16,15 +44,21 @@ class Client(object):
         url: API endpoint URL minus the /<version> part.
         parameters: API parameters to be sent with the request.
         """
-        url = urljoin(self.base_url, url) + '.json'
-        r = self.session.request(method, url, data=parameters)
+        url = '%s.%s' % (urljoin(self.base_url, url), self.response_format)
 
-        if r.error:
-            print 'HTTPError: ' + str(r.error)
-            raise Error('API request failed. URL=%s' % url, http_error=r.error)
+        try:
+            r = self.session.request(method, url, data=parameters)
+        except requests.exceptions.RequestException, e:
+            raise TweepyError('Request error: %s' % e)
 
-        # TODO: parse response
-        return r.content
+        if r.status_code != 200:
+            error_msg = self.parser.parse_error(r.content)
+            raise TweepyError('API error: %s' % error_msg)
+
+        if self.parser:
+            return self.parser.parse_content(r.content)
+        else:
+            return r.content
 
     def public_timeline(self, **parameters):
         """GET statuses/public_timeline"""
--- tweepy/parsers.py
+++ tweepy/parsers.py
@@ -1,85 +1,36 @@
-# Tweepy
-# Copyright 2009-2010 Joshua Roesslein
-# See LICENSE for details.
+from tweepy import TweepyError
 
-from tweepy.models import ModelFactory
-from tweepy.utils import import_simplejson
-from tweepy.error import TweepError
-
+try:
+    import simplejson as json
+except ImportError, e:
+    import json
 
 class Parser(object):
 
-    def parse(self, method, payload):
-        """
-        Parse the response payload and return the result.
-        Returns a tuple that contains the result data and the cursors
-        (or None if not present).
-        """
+    def parse_content(self, content):
+        """Parse the response content and return the a result."""
         raise NotImplementedError
 
-    def parse_error(self, payload):
-        """
-        Parse the error message from payload.
-        If unable to parse the message, throw an exception
-        and default error message will be used.
-        """
+    def parse_error(self, content):
+        """Parse the error response and return a message describing it."""
         raise NotImplementedError
-
 
 class JSONParser(Parser):
+    """Parses JSON formatted responses."""
 
-    payload_format = 'json'
+    accepted_formats = ['json']
 
-    def __init__(self):
-        self.json_lib = import_simplejson()
-
-    def parse(self, method, payload):
+    def parse_content(self, content):
         try:
-            json = self.json_lib.loads(payload)
-        except Exception, e:
-            raise TweepError('Failed to parse JSON payload: %s' % e)
+            result = json.loads(content)
+        except ValueError, e:
+            raise TweepyError('JSON parse error: %s\nJSON content:\n%s' % (e, content))
 
-        needsCursors = method.parameters.has_key('cursor')
-        if needsCursors and isinstance(json, dict) and 'previous_cursor' in json and 'next_cursor' in json:
-            cursors = json['previous_cursor'], json['next_cursor']
-            return json, cursors
-        else:
-            return json
+        return result
 
-    def parse_error(self, payload):
-        error = self.json_lib.loads(payload)
-        if error.has_key('error'):
-            return error['error']
-        else:
-            return error['errors']
+    def parse_error(self, content):
+        error = json.loads(content)
+        return error.get('error', 'Unknown: %s' % content)
 
-
-class ModelParser(JSONParser):
-
-    def __init__(self, model_factory=None):
-        JSONParser.__init__(self)
-        self.model_factory = model_factory or ModelFactory
-
-    def parse(self, method, payload):
-        try:
-            if method.payload_type is None: return
-            model = getattr(self.model_factory, method.payload_type)
-        except AttributeError:
-            raise TweepError('No model for this payload type: %s' % method.payload_type)
-
-        json = JSONParser.parse(self, method, payload)
-        if isinstance(json, tuple):
-            json, cursors = json
-        else:
-            cursors = None
-
-        if method.payload_list:
-            result = model.parse_list(method.api, json)
-        else:
-            result = model.parse(method.api, json)
-
-        if cursors:
-            return result, cursors
-        else:
-            return result
+default_json_parser = JSONParser()
 
